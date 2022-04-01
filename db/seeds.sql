c6713240470060cd2130f58a20769a1443cfafd9
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-04-01 16:26:16 -0400

    started framing out the seeds.sql and the schema.sql

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department (dept)
+VALUES ("Management"), 
+    ("Finance"),
+    ("Operations"),
+    ("Information Technology"),
+    ("Administration"),
+    ("Transportation")
+
+INSERT INTO roles (jobTitle)
+VALUES ("Manager"),
+    ("Accountant"),
+    ("Plant Operator"),
+    ("Desktop Support Specialist"),
+    ("Front Desk Admin"),
+    ("Dispatcher")
+
+INSERT INTO employees (employee)
+VALUES ("Rick Flare"),
+    ("Doug Funny"),
+    ("Alice Wonderland"),
+    ("Billiam Klinton"),
+    ("Linda Hand")
+
--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const express = require('express');
 const inquirer = require('inquirer');
 
 const PORT = 3001;
-const app = express();
+const app = express()
