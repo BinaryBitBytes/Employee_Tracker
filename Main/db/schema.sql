4f5843fd9c73f1afa5ca5741aa2dd5265b4b8551
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-05-09 11:24:52 -0400

    added many files

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -1,18 +1,18 @@
-DROP DATABASE IF EXISTS department_db, roles_db, employees_db;
-CREATE DATABASE department_db, roles_db, employees_db;
+DROP DATABASE IF EXISTS Company_db;/*department_db, roles_db, employees_db;*/
+CREATE DATABASE Company_db;/*department_db, roles_db, employees_db;*/
 
-USE department_db, roles_db, employees_db;
+USE Company_db;/*department_db, roles_db, employees_db;*/
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id PRIMARY KEY INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(18) NOT NULL
 )
 
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id PRIMARY KEY INT NOT NULL AUTO_INCREMENT,
     name VARCHAR (18) NOT NULL
 )
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id PRIMARY KEY INT NOT NULL AUTO_INCREMENT,
     name VARCHAR (22) NOT NULL
